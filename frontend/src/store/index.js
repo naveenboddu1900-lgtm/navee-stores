0623d703b7a1d793de498140f59c51f11fc2db96
Naveen kumar boddu <naveenboddu1900@gmail.com>
2026-06-19 20:00:59 +0530

    Persist cart state locally

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './cartSlice'
 
+const CART_STORAGE_KEY = 'marketplace_cart'
+
+function loadCartState() {
+  try {
+    const raw = localStorage.getItem(CART_STORAGE_KEY)
+    return raw ? { cart: JSON.parse(raw) } : undefined
+  } catch {
+    return undefined
+  }
+}
+
 export const store = configureStore({
   reducer: {
     cart: cartReducer
-  }
+  },
+  preloadedState: loadCartState()
+})
+
+store.subscribe(() => {
+  localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(store.getState().cart))
 })
